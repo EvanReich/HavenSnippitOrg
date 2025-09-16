25202e7df7e95d81fe31db175d6d9e1488e3c3e7
EvanReich <evan.m.reich@gmail.com>
2025-09-16 13:46:34 -0400

    Update ITLCProb003_CASE026_ref.sv

--- ITLCProb003_CASE026_ref.sv
+++ ITLCProb003_CASE026_ref.sv
@@ -12,7 +12,7 @@ module CASE026
             3'b011 : F = 1'b0;
             3'b100 : F = 1'b0;
             3'b101 : F = 1'b0;
-            3'b110 : F = 1'b0;
+            3'b110 : F = 1'b1;
             3'b111 : F = 1'b0;
             default: F = 1'bX; 
         endcase
