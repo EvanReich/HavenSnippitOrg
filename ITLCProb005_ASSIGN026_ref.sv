b87846f0b9558eecf83480e88ea0507691739c40
EvanReich <evan.m.reich@gmail.com>
2025-10-06 13:46:23 -0400

    Create ITLCProb005_ASSIGN026_ref.sv

---
+++ ITLCProb005_ASSIGN026_ref.sv
@@ -0,0 +1,8 @@
+module SystemX (output wire F,
+                input wire A, B, C);
+
+  assign F = ((A == 1'b0) && (B == 1'b0) && (C == 1'b0)) ? 1'b1 :
+             ((A == 1'b0) && (B == 1'b1) && (C == 1'b0)) ? 1'b1 :
+             ((A == 1'b1) && (B == 1'b0) && (C == 1'b0)) ? 1'b1 :
+              1'b0;
+endmodule
