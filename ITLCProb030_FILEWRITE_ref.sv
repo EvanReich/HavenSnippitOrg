cab3e622b1a0c85d384297e1d3ed3d4821ecfdd6
EvanReich <evan.m.reich@gmail.com>
2025-10-15 16:20:10 -0400

    Add FILE_WRITE_INPUTS module for file writing

---
+++ ITLCProb030_FILEWRITE_ref.sv
@@ -0,0 +1,13 @@
+module FILE_WRITE_INPUTS (
+    input integer A,
+    input real B
+);
+    integer FILE_1;
+
+    initial begin
+        FILE_1 = $fopen("Data_out.txt", "w");
+        $fdisplay(FILE_1, "A is %d", A);
+        $fdisplay(FILE_1, "B is %f", B);
+        $fclose(FILE_1);
+    end
+endmodule
