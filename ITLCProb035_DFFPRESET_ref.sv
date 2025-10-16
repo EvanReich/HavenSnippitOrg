32ae38db9ba18dedff1ecd3856d9e5a9b6657efc
EvanReich <evan.m.reich@gmail.com>
2025-10-16 14:15:05 -0400

    Add DFFRESET module with asynchronous reset and preset

---
+++ ITLCProb035_DFFPRESET_ref.sv
@@ -0,0 +1,23 @@
+module DFFRESET (
+    output reg Q, Qn,
+    input wire Clock, Reset, Preset, D
+);
+
+always @ (posedge Clock or negedge Reset or negedge Preset)
+    if (!Reset)
+    begin
+        Q <= 1'b0;
+        Qn <= 1'b1;
+    end
+    else if (!Preset)
+    begin
+        Q <= 1'b1;
+        Qn <= 1'b0;
+    end
+    else
+    begin
+        Q <= D;
+        Qn <= ~D;
+    end
+
+endmodule
