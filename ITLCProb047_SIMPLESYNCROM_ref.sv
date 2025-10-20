b7c2e03e7ee81b4304787c83bb8ae4a7dd18abc0
EvanReich <evan.m.reich@gmail.com>
2025-10-20 14:12:31 -0400

    Add SIMPLESYNCROM module with addressable outputs

---
+++ ITLCProb047_SIMPLESYNCROM_ref.sv
@@ -0,0 +1,16 @@
+module SIMPLESYNCROM (
+    output reg [3:0] data_out,
+    input wire [1:0] address,
+    input wire Clock
+);
+
+always @ (posedge Clock)
+    case (address)
+        0       : data_out = 4'b1110;
+        1       : data_out = 4'b0010;
+        2       : data_out = 4'b1111;
+        3       : data_out = 4'b1010;
+        default : data_out = 4'bXXXX;
+    endcase
+
+endmodule
